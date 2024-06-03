6274c2c5ff40276282fedb3ac5d5f9e4454be41e
adlizone <aadilazizlone647@gmail.com>
2024-06-02 22:33:26 -0700

    ...

--- kurooms/listings/models.py
+++ kurooms/listings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Property(models.Model):
+    title = models.CharField(maxlength=100)
+    description = models.TextField()
+    address = models.CharField(maxlength=200)
+    contact_1 = models.CharField(maxlength=10)
+    contact_2 = models.CharField(maxlength=10)
+
+    owner = models.ForeignKey(setting.AUTH_USER_MODEL, ondelete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title 
